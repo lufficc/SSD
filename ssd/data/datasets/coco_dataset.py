17ef3de5e1fc3b10bd8ba4def777b5d6e38eb362
lufficc <luffy.lcc@gmail.com>
2019-03-13 16:42:39 +0800

    fix #46

--- ssd/data/datasets/coco_dataset.py
+++ ssd/data/datasets/coco_dataset.py
@@ -7,7 +7,7 @@ from PIL import Image
 class COCODataset(torch.utils.data.Dataset):
     class_names = ('__background__',
                    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-                   'train', 'truck', 'boat', 'traffic light', 'fire', 'hydrant',
+                   'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
                    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
                    'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra',
                    'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
@@ -18,7 +18,7 @@ class COCODataset(torch.utils.data.Dataset):
                    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
                    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
                    'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-                   'keyboard', 'cell phone', 'microwave oven', 'toaster', 'sink',
+                   'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
                    'refrigerator', 'book', 'clock', 'vase', 'scissors',
                    'teddy bear', 'hair drier', 'toothbrush')
 
