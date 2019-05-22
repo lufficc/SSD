b14cad4e9cfd0a6d47199fa8d188312c7fa5b911
lufficc <luffy.lcc@gmail.com>
2019-05-22 14:10:20 +0800

    update WarmupMultiStepLR

--- ssd/utils/lr_scheduler.py
+++ ssd/utils/lr_scheduler.py
@@ -9,9 +9,10 @@ class WarmupMultiStepLR(MultiStepLR):
         super().__init__(optimizer, milestones, gamma, last_epoch)
 
     def get_lr(self):
-        lr = super().get_lr()
-        if self.last_epoch < self.warmup_iters:
+        if self.last_epoch <= self.warmup_iters:
             alpha = self.last_epoch / self.warmup_iters
             warmup_factor = self.warmup_factor * (1 - alpha) + alpha
-            return [l * warmup_factor for l in lr]
+            return [lr * warmup_factor for lr in self.base_lrs]
+        else:
+            lr = super().get_lr()
         return lr
