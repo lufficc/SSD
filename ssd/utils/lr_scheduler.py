ea428b9edb222afbd194b0d369e4ed2b29a5ee06
Congcong Li <luffy.lcc@gmail.com>
2019-05-22 14:07:19 +0800

    Revert "update lr_scheduler"

--- ssd/utils/lr_scheduler.py
+++ ssd/utils/lr_scheduler.py
@@ -1,5 +1,3 @@
-from bisect import bisect_right
-
 from torch.optim.lr_scheduler import MultiStepLR
 
 
@@ -8,15 +6,12 @@ class WarmupMultiStepLR(MultiStepLR):
                  warmup_iters=500, last_epoch=-1):
         self.warmup_factor = warmup_factor
         self.warmup_iters = warmup_iters
-        super(MultiStepLR, self).__init__(optimizer, milestones, gamma, last_epoch)
-        self.milestones = milestones
-        self.last_epoch = 0
+        super().__init__(optimizer, milestones, gamma, last_epoch)
 
     def get_lr(self):
-        lr = self.base_lrs
-        warmup_factor = 1
+        lr = super().get_lr()
         if self.last_epoch < self.warmup_iters:
             alpha = self.last_epoch / self.warmup_iters
             warmup_factor = self.warmup_factor * (1 - alpha) + alpha
             return [l * warmup_factor for l in lr]
-        return [l * warmup_factor * self.gamma ** bisect_right(self.milestones, self.last_epoch) for l in lr]
+        return lr
