f4611e1d12f6346c8c77e5f20a036a4ff784db91
lufficc <luffy.lcc@gmail.com>
2019-06-23 22:33:08 +0800

    optimize demo

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 import glob
 import os
+import time
 
 import torch
 from PIL import Image
-from tqdm import tqdm
 from vizer.draw import draw_boxes
 
 from ssd.config import cfg
@@ -31,6 +31,8 @@ def run_demo(cfg, ckpt, score_threshold, images_dir, output_dir, dataset_type):
     model = model.to(device)
     checkpointer = CheckPointer(model, save_dir=cfg.OUTPUT_DIR)
     checkpointer.load(ckpt, use_latest=ckpt is None)
+    weight_file = ckpt if ckpt else checkpointer.get_checkpoint_file()
+    print('Loaded weights from {}'.format(weight_file))
 
     image_paths = glob.glob(os.path.join(images_dir, '*.jpg'))
     mkdir(output_dir)
@@ -38,24 +40,38 @@ def run_demo(cfg, ckpt, score_threshold, images_dir, output_dir, dataset_type):
     cpu_device = torch.device("cpu")
     transforms = build_transforms(cfg, is_train=False)
     model.eval()
-    for image_path in tqdm(image_paths):
-        image = np.array(Image.open(image_path).convert("RGB"))
-        height, width, _ = image.shape
-        images = transforms(image)[0].unsqueeze(0)
+    for i, image_path in enumerate(image_paths):
+        start = time.time()
+        image_name = os.path.basename(image_path)
 
+        image = np.array(Image.open(image_path).convert("RGB"))
+        height, width = image.shape[:2]
+        images = transforms(image)[0].unsqueeze(0)
+        load_time = time.time() - start
+
+        start = time.time()
         result = model(images.to(device))[0]
+        inference_time = time.time() - start
+
         result = result.resize((width, height)).to(cpu_device).numpy()
         boxes, labels, scores = result['boxes'], result['labels'], result['scores']
 
         indices = scores > score_threshold
-
         boxes = boxes[indices]
         labels = labels[indices]
         scores = scores[indices]
+        meters = ' | '.join(
+            [
+                'objects {:02d}'.format(len(boxes)),
+                'load {:03d}ms'.format(round(load_time * 1000)),
+                'inference {:03d}ms'.format(round(inference_time * 1000)),
+                'FPS {}'.format(round(1.0 / inference_time))
+            ]
+        )
+        print('({:04d}/{:04d}) {}: {}'.format(i + 1, len(image_paths), image_name, meters))
 
         drawn_image = draw_boxes(image, boxes, labels, scores, class_names).astype(np.uint8)
-        image_name = os.path.basename(image_path)
-        Image.fromarray(drawn_image).save(os.path.join(output_dir, '{}_demo.jpg'.format(image_name.split('.')[0])))
+        Image.fromarray(drawn_image).save(os.path.join(output_dir, image_name))
 
 
 def main():
@@ -68,7 +84,7 @@ def main():
         type=str,
     )
     parser.add_argument("--ckpt", type=str, default=None, help="Trained weights.")
-    parser.add_argument("--score_threshold", type=float, default=0.5)
+    parser.add_argument("--score_threshold", type=float, default=0.7)
     parser.add_argument("--images_dir", default='demo', type=str, help='Specify a image dir to do prediction.')
     parser.add_argument("--output_dir", default='demo/result', type=str, help='Specify a image dir to save predicted images.')
     parser.add_argument("--dataset_type", default="voc", type=str, help='Specify dataset type. Currently support voc and coco.')
