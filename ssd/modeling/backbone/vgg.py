6385e86caad356fa7afee4056cd36cf2b533113c
lufficc <luffy.lcc@gmail.com>
2019-07-15 11:00:18 +0800

    remove unused add_header

--- ssd/modeling/backbone/vgg.py
+++ ssd/modeling/backbone/vgg.py
@@ -53,23 +53,6 @@ def add_extras(cfg, i, size=300):
     return layers
 
 
-def add_header(vgg, extra_layers, boxes_per_location, num_classes):
-    regression_headers = []
-    classification_headers = []
-    vgg_source = [21, -2]
-    for k, v in enumerate(vgg_source):
-        regression_headers += [nn.Conv2d(vgg[v].out_channels,
-                                         boxes_per_location[k] * 4, kernel_size=3, padding=1)]
-        classification_headers += [nn.Conv2d(vgg[v].out_channels,
-                                             boxes_per_location[k] * num_classes, kernel_size=3, padding=1)]
-    for k, v in enumerate(extra_layers[1::2], 2):
-        regression_headers += [nn.Conv2d(v.out_channels, boxes_per_location[k]
-                                         * 4, kernel_size=3, padding=1)]
-        classification_headers += [nn.Conv2d(v.out_channels, boxes_per_location[k]
-                                             * num_classes, kernel_size=3, padding=1)]
-    return regression_headers, classification_headers
-
-
 vgg_base = {
     '300': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
             512, 512, 512],
