51c1768399bbcdaf5d3207537b9d32fcdd7fe37e
lufficc <luffy.lcc@gmail.com>
2020-01-15 20:37:43 +0800

    add batched_nms, which is more faster when run inference

--- ssd/modeling/box_head/inference.py
+++ ssd/modeling/box_head/inference.py
@@ -1,7 +1,7 @@
 import torch
 
 from ssd.structures.container import Container
-from ssd.utils.nms import boxes_nms
+from ssd.utils.nms import batched_nms
 
 
 class PostProcessor:
@@ -17,46 +17,37 @@ class PostProcessor:
         batch_size = batches_scores.size(0)
         results = []
         for batch_id in range(batch_size):
-            processed_boxes = []
-            processed_scores = []
-            processed_labels = []
+            scores, boxes = batches_scores[batch_id], batches_boxes[batch_id]  # (N, #CLS) (N, 4)
+            num_boxes = scores.shape[0]
+            num_classes = scores.shape[1]
 
-            per_img_scores, per_img_boxes = batches_scores[batch_id], batches_boxes[batch_id]  # (N, #CLS) (N, 4)
-            for class_id in range(1, per_img_scores.size(1)):  # skip background
-                scores = per_img_scores[:, class_id]
-                mask = scores > self.cfg.TEST.CONFIDENCE_THRESHOLD
-                scores = scores[mask]
-                if scores.size(0) == 0:
-                    continue
-                boxes = per_img_boxes[mask, :]
-                boxes[:, 0::2] *= self.width
-                boxes[:, 1::2] *= self.height
+            boxes = boxes.view(num_boxes, 1, 4).expand(num_boxes, num_classes, 4)
+            labels = torch.arange(num_classes, device=device)
+            labels = labels.view(1, num_classes).expand_as(scores)
 
-                keep = boxes_nms(boxes, scores, self.cfg.TEST.NMS_THRESHOLD, self.cfg.TEST.MAX_PER_CLASS)
+            # remove predictions with the background label
+            boxes = boxes[:, 1:]
+            scores = scores[:, 1:]
+            labels = labels[:, 1:]
 
-                nmsed_boxes = boxes[keep, :]
-                nmsed_labels = torch.tensor([class_id] * keep.size(0), device=device)
-                nmsed_scores = scores[keep]
+            # batch everything, by making every class prediction be a separate instance
+            boxes = boxes.reshape(-1, 4)
+            scores = scores.reshape(-1)
+            labels = labels.reshape(-1)
 
-                processed_boxes.append(nmsed_boxes)
-                processed_scores.append(nmsed_scores)
-                processed_labels.append(nmsed_labels)
+            # remove low scoring boxes
+            indices = torch.nonzero(scores > self.cfg.TEST.CONFIDENCE_THRESHOLD).squeeze(1)
+            boxes, scores, labels = boxes[indices], scores[indices], labels[indices]
 
-            if len(processed_boxes) == 0:
-                processed_boxes = torch.empty(0, 4)
-                processed_labels = torch.empty(0)
-                processed_scores = torch.empty(0)
-            else:
-                processed_boxes = torch.cat(processed_boxes, 0)
-                processed_labels = torch.cat(processed_labels, 0)
-                processed_scores = torch.cat(processed_scores, 0)
+            boxes[:, 0::2] *= self.width
+            boxes[:, 1::2] *= self.height
 
-            if processed_boxes.size(0) > self.cfg.TEST.MAX_PER_IMAGE > 0:
-                processed_scores, keep = torch.topk(processed_scores, k=self.cfg.TEST.MAX_PER_IMAGE)
-                processed_boxes = processed_boxes[keep, :]
-                processed_labels = processed_labels[keep]
+            keep = batched_nms(boxes, scores, labels, self.cfg.TEST.NMS_THRESHOLD)
+            # keep only topk scoring predictions
+            keep = keep[:self.cfg.TEST.MAX_PER_IMAGE]
+            boxes, scores, labels = boxes[keep], scores[keep], labels[keep]
 
-            container = Container(boxes=processed_boxes, labels=processed_labels, scores=processed_scores)
+            container = Container(boxes=boxes, labels=labels, scores=scores)
             container.img_width = self.width
             container.img_height = self.height
             results.append(container)
--- ssd/utils/nms.py
+++ ssd/utils/nms.py
@@ -16,18 +16,55 @@ except ImportError:
         sys.exit(-1)
 
 
-def boxes_nms(boxes, scores, nms_thresh, max_count=-1):
+def nms(boxes, scores, nms_thresh):
     """ Performs non-maximum suppression, run on GPU or CPU according to
     boxes's device.
     Args:
-        boxes(Tensor): `xyxy` mode boxes, use absolute coordinates(or relative coordinates), shape is (n, 4)
-        scores(Tensor): scores, shape is (n, )
+        boxes(Tensor[N, 4]): boxes in (x1, y1, x2, y2) format, use absolute coordinates(or relative coordinates)
+        scores(Tensor[N]): scores
         nms_thresh(float): thresh
-        max_count (int): if > 0, then only the top max_proposals are kept  after non-maximum suppression
     Returns:
         indices kept.
     """
     keep = _nms(boxes, scores, nms_thresh)
-    if max_count > 0:
-        keep = keep[:max_count]
+    return keep
+
+
+def batched_nms(boxes, scores, idxs, iou_threshold):
+    """
+    Performs non-maximum suppression in a batched fashion.
+
+    Each index value correspond to a category, and NMS
+    will not be applied between elements of different categories.
+
+    Parameters
+    ----------
+    boxes : Tensor[N, 4]
+        boxes where NMS will be performed. They
+        are expected to be in (x1, y1, x2, y2) format
+    scores : Tensor[N]
+        scores for each one of the boxes
+    idxs : Tensor[N]
+        indices of the categories for each one of the boxes.
+    iou_threshold : float
+        discards all overlapping boxes
+        with IoU < iou_threshold
+
+    Returns
+    -------
+    keep : Tensor
+        int64 tensor with the indices of
+        the elements that have been kept by NMS, sorted
+        in decreasing order of scores
+    """
+    if boxes.numel() == 0:
+        return torch.empty((0,), dtype=torch.int64, device=boxes.device)
+    # strategy: in order to perform NMS independently per class.
+    # we add an offset to all the boxes. The offset is dependent
+    # only on the class idx, and is large enough so that boxes
+    # from different classes do not overlap
+    max_coordinate = boxes.max()
+    offsets = idxs.to(boxes) * (max_coordinate + 1)
+    boxes_for_nms = boxes + offsets[:, None]
+    keep = nms(boxes_for_nms, scores, iou_threshold)
     return keep
